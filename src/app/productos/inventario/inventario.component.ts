6f4df357d5117f7a301176cf2db781d7b0cb367a
Jorge Ayala <jorge.ayala@itsense.com.co>
2025-03-20 09:35:48 -0500

    Creación de front, sin plantillas html iniciales

---
+++ src/app/productos/inventario/inventario.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from '../productos.service';
+
+@Component({
+  selector: 'app-inventario',
+  templateUrl: './inventario.component.html',
+  styleUrls: ['./inventario.component.css']
+})
+export class InventarioComponent implements OnInit {
+  productos: any[] = [];
+
+  constructor(private productosService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productosService.getInventario().subscribe(
+      (response) => {
+        this.productos = response;
+      },
+      (error) => {
+        console.error('Error al obtener el inventario', error);
+      }
+    );
+  }
+}
